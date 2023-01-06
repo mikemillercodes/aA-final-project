767e2c167aac63835e89f2160eb6383982f2e0e8
Mike Miller <millermichaelg1@gmail.com>
2023-01-05 20:03:02 -0600

    login form styled!

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,32 +32,48 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
+    <form classname='login-form' onSubmit={onLogin}>
+      <div className='errors'>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
+      <div className='login-outer'>
+
+        <div className='login-labels'>
+          <div className='email-input'>
+
+          </div>
+
+          <div className='email-input-outer'>
+            <label htmlFor='email'>Email</label>
+            <input
+            required
+              className='email-input'
+              name='email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+
+          <div className='password-input-outer'>
+            <label htmlFor='password'>Password</label>
+            <input
+            required
+              className='password-input'
+              name='password'
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+
+        <button id='login-btn' type='submit'>Login</button>
+        </div>
+
       </div>
     </form>
   );
